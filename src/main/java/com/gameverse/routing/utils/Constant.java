7a6bc0714a6acfb35c2f81b5699080aedeec9f41
pawan <pawanpatel12895@gmail.com>
2023-06-09 13:43:31 +0530

    Added a Constant File

---
+++ src/main/java/com/gameverse/routing/utils/Constant.java
@@ -0,0 +1,11 @@
+package com.gameverse.routing.utils;
+
+public final class Constant {
+    public final class Cache {
+        public static final String CACHE_NAME = "routingCache";
+    }
+
+    public final class Routing {
+        public static final String ROUTING_STORE_JSON_FILE = "routingFile.json";
+    }
+}
